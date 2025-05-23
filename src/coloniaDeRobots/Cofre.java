e404841a68b9e2835bc2ce6aa35dd7034f2e4d6a
Lautaro <lautarocasas12@gmail.com>
2025-05-23 18:23:57 -0300

    Agregado sistema de gestion de inventarios

--- src/coloniaDeRobots/Cofre.java
+++ src/coloniaDeRobots/Cofre.java
@@ -1,6 +1,8 @@
 package coloniaDeRobots;
+import java.util.Map;
 
 public abstract class Cofre {
 	private int coordX;
 	private int coordY;
+	private Map<String,Integer> inventario; //Mapa que asigna a cada itemId una cantidad
 }
