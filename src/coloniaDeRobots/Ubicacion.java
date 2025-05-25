48e7af11f0c679d4949fe8c7e625819c24d621de
Lautaro <lautarocasas12@gmail.com>
2025-05-25 20:50:40 -0300

    Agregada clase Ubicacion

---
+++ src/coloniaDeRobots/Ubicacion.java
@@ -0,0 +1,11 @@
+package coloniaDeRobots;
+
+public class Ubicacion {
+	int coordX;
+	int coordY;
+	
+	public Ubicacion(int coordX,int coordY) {
+		this.coordX = coordX;
+		this.coordY = coordY;
+	}
+}
