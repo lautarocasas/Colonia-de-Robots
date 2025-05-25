fa41f7f9aa62b8d73c6dc8f1e8e1ff0df23fd892
Lautaro <lautarocasas12@gmail.com>
2025-05-25 20:55:21 -0300

    Agregado atributo Ubicacion a la clase Cofre

--- src/coloniaDeRobots/Cofres/Cofre.java
+++ src/coloniaDeRobots/Cofres/Cofre.java
@@ -1,14 +1,13 @@
 package coloniaDeRobots.Cofres;
 import java.util.HashMap;
+import coloniaDeRobots.Ubicacion;
 
 public abstract class Cofre {
-	private int coordX;
-	private int coordY;
+	private Ubicacion ubicacion;
 	private HashMap<String,Integer> inventario; //Mapa que asigna a cada itemId una cantidad
 	
 	Cofre(int coordX,int coordY){
-		this.coordX = coordX;
-		this.coordY = coordY;
+		this.ubicacion = new Ubicacion(coordX,coordY);
 		this.inventario = new HashMap<String,Integer>();
 	}
 }
