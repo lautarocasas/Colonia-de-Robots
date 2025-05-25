4358bcbe32212e7c72207f13d70c687f0f2bf204
Lautaro <lautarocasas12@gmail.com>
2025-05-25 20:54:50 -0300

    Agregado constructor y getters a clase Ubicacion

--- src/coloniaDeRobots/Ubicacion.java
+++ src/coloniaDeRobots/Ubicacion.java
@@ -1,11 +1,19 @@
 package coloniaDeRobots;
 
 public class Ubicacion {
-	int coordX;
-	int coordY;
+	private int coordX;
+	private int coordY;
 	
 	public Ubicacion(int coordX,int coordY) {
 		this.coordX = coordX;
 		this.coordY = coordY;
+	}
+	
+	public int getX() {
+		return this.coordX;
+	}
+	
+	public int getY() {
+		return this.coordY;
 	}
 }
