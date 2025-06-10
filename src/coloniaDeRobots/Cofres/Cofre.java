7d30267e8777ca86bf65dfd363fcb6f917bf4883
Lautaro <lautarocasas12@gmail.com>
2025-06-10 20:33:38 -0300

    Agregados metodos generales a la clase Cofre

--- src/coloniaDeRobots/Cofres/Cofre.java
+++ src/coloniaDeRobots/Cofres/Cofre.java
@@ -1,13 +1,39 @@
 package coloniaDeRobots.Cofres;
 import java.util.HashMap;
-import coloniaDeRobots.Ubicacion;
+
+import coloniaDeRobots.*;
 
 public abstract class Cofre {
-	private Ubicacion ubicacion;
-	private HashMap<String,Integer> inventario; //Mapa que asigna a cada itemId una cantidad
+	protected Ubicacion ubicacion;
+	protected HashMap<Item,Integer> inventario; //Mapa que asigna a cada itemId una cantidad
 	
 	Cofre(int coordX,int coordY){
 		this.ubicacion = new Ubicacion(coordX,coordY);
-		this.inventario = new HashMap<String,Integer>();
+		this.inventario = new HashMap<Item,Integer>();
 	}
+	
+	public void accion(SistemaLogistico sistema) {
+		//El cofre debe tener acceso al estado general del sistema
+	}
+	
+	public int getCantidadItem(Item item) {
+		int cant = this.inventario.getOrDefault(item,0);	// Si existe el item en el inventario, se obtiene la cantidad, sino, se obtiene 0
+		return cant;
+	}
+	
+    public boolean retirarItem(Item item, int cantidad) {
+        int cantActual = this.getCantidadItem(item);
+        if (cantActual >= cantidad) {
+        	inventario.put(item, cantActual - cantidad);
+        	if (this.getCantidadItem(item) == 0) //Si al restar la cantidad solicitada no queda ningun item, se elimina el item del mapa
+        		inventario.remove(item);
+        	return true;        	
+        }else        
+        	return false;
+    }
+    
+    public void agregarItem(Item item, int cantidad) {
+    	int cantActual = this.getCantidadItem(item);
+    	inventario.put(item, cantActual+ cantidad);
+    }
 }
--- src/coloniaDeRobots/Cofres/CofreProvisionActiva.java
+++ src/coloniaDeRobots/Cofres/CofreProvisionActiva.java
@@ -1,7 +1,28 @@
 package coloniaDeRobots.Cofres;
 
+import java.util.List;
+
+import coloniaDeRobots.*;
+
 public class CofreProvisionActiva extends Cofre {
 	public CofreProvisionActiva(int coordX,int coordY){
 		super(coordX,coordY);
 	}
+	
+	@Override
+    public void accion(SistemaLogistico sistema) {
+        // Buscar solicitudes en el sistema para ítems que yo tengo
+        List<Solicitud> solicitudes = sistema.obtenerSolicitudesPendientes();
+
+        for (Solicitud s : solicitudes) {
+        	Item itemSolicitado = s.getItem();
+        	int cantidadDisponible = this.getCantidadItem(itemSolicitado);
+        	
+        	if(cantidadDisponible > 0 && s.getCantidadPendiente() > 0 ) {
+        		//Si tengo stock del item que me pidieron y faltan items para completar la solicitud, envio la cantidad necesaria
+        		int cantidadAEnviar = Math.min(cantidadDisponible, s.getCantidadPendiente());
+        		
+        	}
+        }
+}
 }
